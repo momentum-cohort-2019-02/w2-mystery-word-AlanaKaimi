74f006e2d11b9d7977208f293145940c8fbc2138
AlanaKaimi <ajbryce@live.com>
2019-02-21 14:12:11 -0500

    I finally made a Function and it works!!!

--- mystery_word.py
+++ mystery_word.py
@@ -1,9 +1,11 @@
-print("""Welcome to Mystery Word!""" )
-print("""Choose your level of difficulty:
+print("""Welcome to Mystery Word!
+    Choose your level of difficulty:
     Easy: 4-6 characters
     Normal: 6-8 characters
     Hard: 8+ characters""")
 # * works!
+
+
 
 # Let user choose dificulty level of easy: 4-6 char normal: 6-8 char, and Hard 8+ char
     #  Read words from a file words.txt
@@ -11,19 +13,22 @@ print("""Choose your level of difficulty:
         # filter a list by string size
     # get random entry from list
         #random module? 
-
-# def choose_difficulty(diff_choice): 
-   
-
 diff_choice = input("Difficulty level: ")
-if diff_choice = ("Easy")
-    print(diff_choice + "4-6")
-elif diff_choice = ("Normal") 
-    print(diff_choice + "6-8")
-else diff_choice = ("Hard")
-    print(diff_choice + "8+")
 
+def choose_difficulty(diff_choice): 
+    
+    diff_choice = diff_choice.casefold()
+    if diff_choice == ("Easy"):
+        print(diff_choice + " 4-6 letters")
+    elif diff_choice == ("Normal"): 
+        print(diff_choice + " 6-8 letters")
+    elif diff_choice == ("Hard"):
+        print(diff_choice + " 8+ letters")
+        
+# * works!
 
+choose_difficulty (diff_choice)
+# * works!
 
 # Print how many chars the word contains
 
