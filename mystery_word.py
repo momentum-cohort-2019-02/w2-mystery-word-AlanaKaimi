120553373d4c5fff41742170bcbd26bf885b70b9
AlanaKaimi <ajbryce@live.com>
2019-02-23 15:25:01 -0500

    Can generate a random word from the list! eh... more notes than code :/ but something werks!

--- mystery_word.py
+++ mystery_word.py
@@ -1,11 +1,12 @@
-print("""Welcome to Mystery Word!
-    Choose your level of difficulty:
-    Easy: 4-6 characters
-    Normal: 6-8 characters
-    Hard: 8+ characters""")
-# * works!
+import random
+###TODO Most of it! Sorry for the mess!
 
-
+# print("""Welcome to Mystery Word!
+#     Choose your level of difficulty:
+#     Easy: 4-6 characters
+#     Normal: 6-8 characters
+#     Hard: 8+ characters""")
+# ### * WORKS!
 
 # Let user choose dificulty level of easy: 4-6 char normal: 6-8 char, and Hard 8+ char
     #  Read words from a file words.txt
@@ -13,22 +14,95 @@ print("""Welcome to Mystery Word!
         # filter a list by string size
     # get random entry from list
         #random module? 
-diff_choice = input("Difficulty level: ")
+# ### * WORKS!
+# diff_choice = input("Difficulty level: ")
+# ### * WORKS!
 
-def choose_difficulty(diff_choice): 
+# ### * WORKS!
+# def choose_difficulty(diff_choice): 
+# # .casefold() allows user to input answer in any case and it will be accepted     
+#     diff_choice = diff_choice.casefold()
+# # responds to user's choice input
+#     if diff_choice == ("easy"):
+#         print(f"{diff_choice} 4-6 letters")
+#     elif diff_choice == ("normal"): 
+#         print(diff_choice + " 6-8 letters")
+#     elif diff_choice == ("hard"):
+#         print(diff_choice + " 8+ letters")
+   
+# ### * WORKS!
+
+# choose_difficulty(diff_choice)
+
+
+###* WORKS 
+# open file and choose a random word 
+def get_random_word(word):
+    word = random.choice(open('words.txt').read().split()).strip()
+    return word
+
+print(get_random_word('word'))
+###* WORKS
+
+###TODO
+def display_word(secret_word):
+    secret_word = get_random_word('word')
+    dashes = "_" * len(secret_word)
+    guesses_left = 8
     
-    diff_choice = diff_choice.casefold()
-    if diff_choice == ("Easy"):
-        print(diff_choice + " 4-6 letters")
-    elif diff_choice == ("Normal"): 
-        print(diff_choice + " 6-8 letters")
-    elif diff_choice == ("Hard"):
-        print(diff_choice + " 8+ letters")
-        
-# * works!
 
-choose_difficulty (diff_choice)
-# * works!
+    # while guesses_left > -1 and not dashes == secret_word:
+    print (len(secret_word))
+    print(dashes)
+    print (str(guesses_left))
+print(display_word('secret_word'))
+
+
+
+# --based off of difficulty level (# of characters)
+
+
+
+# def choose_word(filename):
+#     """Read in `file` and choose word based on difficulty level"""
+    
+#     with open("words.txt") as file:
+#         text = file.read
+
+#         text = normalize_text(text)
+#         words = []
+
+#     for word in text.split(" "):
+#         words.append(word)
+
+#     for word in sorted(words, key=words.get):
+#         easy_words = 
+#         medium_words =
+#         hard_words =
+
+# Find the length of word
+
+# def word_length(word_list):
+#     """Given a list, go through and find the length of a word"""
+#     word_count = {}
+#     for word in list_list:
+#         if word not in word_count:
+#         word_count[word] = 1
+
+#         else:
+#             word_count[word] += 1
+        
+#         return word_count
+        
+# call function
+### * WORKS!
+# choose_difficulty (diff_choice)
+### * WORKS!
+# choose_word (filename)
+
+
+
+    
 
 # Print how many chars the word contains
 
